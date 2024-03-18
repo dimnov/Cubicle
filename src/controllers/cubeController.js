20c30984524ce2061348bf1b24cb4346daf85537
dimnov <dimnov1512@gmail.com>
2024-03-19 00:42:45 +0200

    Update attach page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -32,7 +32,9 @@ router.get('/:id/accessory', async (req, res) => {
   const cube = await cubeManager.getOne(req.params.id).lean();
   const accessories = await accessoryManager.getAll().lean();
 
-  res.render('accessory/attach', { cube, accessories });
+  const hasAccessories = accessories.length > 0;
+
+  res.render('accessory/attach', { cube, accessories, hasAccessories });
 });
 
 module.exports = router;

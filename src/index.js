85afa6d4089059264a4802bd06bb0848277f338c
dimnov <dimnov1512@gmail.com>
2024-03-18 20:49:31 +0200

    Setup mongoose

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const expressConfig = require('./config/express');
 const handlebarsConfig = require('./config/handlebars');
+const dbConnect = require('./config/dbConfig');
+
 const PORT = require('./config/config');
 const routes = require('./routes');
 
@@ -9,6 +11,10 @@ const app = express();
 
 expressConfig(app);
 handlebarsConfig(app);
+
+dbConnect()
+  .then(() => console.log('DB connected...'))
+  .catch((err) => console.log(err));
 
 app.use(routes);
 

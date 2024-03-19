4902938f4926ebbb0f39a6d737f6a3da5fd98fc8
dimnov <dimnov1512@gmail.com>
2024-03-19 12:55:49 +0200

    Add dynamically accessories

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -18,7 +18,7 @@ exports.getAll = async (search, from, to) => {
   return result;
 }
 
-exports.getOne = (cubeId) => Cube.findById(cubeId);
+exports.getOne = (cubeId) => Cube.findById(cubeId).populate('accessories');
 
 exports.create = (cubeData) => Cube.create(cubeData);
 

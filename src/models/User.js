64795b12763d6eb9aa388988e4d37bda5774cf3f
dimnov <dimnov1512@gmail.com>
2024-03-23 18:37:30 +0200

    Hash password before save

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
   username: String,
@@ -11,6 +12,13 @@ userSchema.virtual('repeatPassword')
       throw new mongoose.MongooseError('Password missmatch!');
     }
   });
+
+userSchema.pre('save', async function () {
+  if (this.password) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+  }
+});
 
 const User = mongoose.model('User', userSchema);
 

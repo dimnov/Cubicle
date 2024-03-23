19337dac983dbf9b5a704680c7e9c9359fbbbccc
dimnov <dimnov1512@gmail.com>
2024-03-23 21:51:28 +0200

    Add auth middleware

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 const User = require('../models/User.js');
 const jwt = require('../lib/jwt');
 
-const SECRET = require('../config/config');
+const { SECRET } = require('../config/config');
 
 exports.register = (username, password, repeatPassword) => User.create(username, password, repeatPassword);
 

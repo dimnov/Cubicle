0635ef0b38b42a2ca951520671a287f6c6d96242
dimnov <dimnov1512@gmail.com>
2024-03-23 21:17:59 +0200

    Add cookie parser and set jwt cookie

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,20 +1,28 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User.js');
+const jwt = require('../lib/jwt');
+
+const SECRET = 'superSecret123';
 
 exports.register = (username, password, repeatPassword) => User.create(username, password, repeatPassword);
 
 exports.login = async (username, password) => {
   const user = await User.findOne({ username });
-
   if (!user) {
     throw new Error('Cannot find username or password');
   }
 
   const isValid = await bcrypt.compare(password, user.password);
-
   if (!isValid) {
     throw new Error('Cannot find username or password');
   }
 
-  return user;
+  const payload = {
+    _id: user._id,
+    username: user.username
+  }
+
+  const token = await jwt.sign(payload, SECRET, { expiresIn: '2d' });
+
+  return token;
 }
